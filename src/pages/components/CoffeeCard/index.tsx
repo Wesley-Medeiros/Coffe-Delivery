83a1427332ca776015ce4ee23007a2de301401ac
Wesley Medeiros Ramalho <wesley.medeiros6938@gmail.com>
2023-05-10 17:38:24 -0300

    styles of cards

---
+++ src/pages/components/CoffeeCard/index.tsx
@@ -0,0 +1,25 @@
+import { RegularText, TitleText } from "../../../components/Typograph"
+import { CardFooterContainer, CoffeeCardContainer, Description, Name, Tags } from "./styles"
+
+function CoffeeCard() {
+  return(
+    <CoffeeCardContainer>
+      <img src="https://s3-alpha-sig.figma.com/img/55b1/f9ee/64600f98b2bae456b96fdc624c4b4f47?Expires=1684713600&Signature=A-rWinlrDY3YHUFPZdnjSMiPAP0A2TC1BKGPr9gWOKFHdVYMGOctzWxbYkLNewn8lOSe~qnviAu26dLzzNQMOIHroQMFhw71h~vqCbFhkx1DyPUzlGQjgzdspufplOqKWU6qB9IJgoW5xL8yrKXAhOkKlB3RMaN9l1B79ZyeqzIVrfpqau--XzjYCQzSkIDzqsP9EFZw4g5I8MKXpQGFHcQr~Qyj4AZT1kYkraz5ztyYkqXdmzlDsGSszGmutPe7vWr9N3WVAqe6RfeAqOn3yq0HePW3Dbpj~X94eiJnt2u0WtoiV4rtTVlLfb4DenwOkB1HWWL151QYbGaP99oVIg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" />
+
+      <Tags>
+        <span>Tradicional</span>
+        <span>Com leite</span>
+      </Tags>
+      <Name>Expresso tradicional</Name>
+      <Description>O tradicional café feito com água quente e grãos moídos</Description>
+      <CardFooterContainer>
+        <div>
+          <RegularText size="s">R$</RegularText>
+          <TitleText size="m" color="text" as="strong">9,90</TitleText>
+        </div>
+      </CardFooterContainer>
+    </CoffeeCardContainer>
+  )
+}
+
+export default CoffeeCard
